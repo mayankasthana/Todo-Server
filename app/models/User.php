1ad9a545dd324fe27ece4a45714a6183e6008b86
mayank <mayankasthana1993@gmail.com>
2014-10-19 22:54:34 +0530

    Added User schema migrations, seeder and models

--- app/models/User.php
+++ app/models/User.php
@@ -1,26 +1,19 @@
 <?php
 
-use Illuminate\Auth\UserTrait;
-use Illuminate\Auth\UserInterface;
-use Illuminate\Auth\Reminders\RemindableTrait;
-use Illuminate\Auth\Reminders\RemindableInterface;
+class User extends Eloquent {
 
-class User extends Eloquent implements UserInterface, RemindableInterface {
-
-	use UserTrait, RemindableTrait;
-
-	/**
-	 * The database table used by the model.
-	 *
-	 * @var string
-	 */
 	protected $table = 'users';
+	public $timestamps = true;
+	protected $hidden = array('salt', 'register_ip', 'forget_token', 'active_token');
 
-	/**
-	 * The attributes excluded from the model's JSON form.
-	 *
-	 * @var array
-	 */
-	protected $hidden = array('password', 'remember_token');
+	public function role()
+	{
+		return $this->belongsToMany('Role');
+	}
+
+	public function attempts()
+	{
+		return $this->hasMany('Login_attempts');
+	}
 
 }
