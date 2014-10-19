ceb7ae9c8432de4aea19d176632700971b3595b3
mayank <mayankasthana1993@gmail.com>
2014-10-20 00:22:02 +0530

    Added database seed for 2 roles and 3 users

--- app/database/seeds/DatabaseSeeder.php
+++ app/database/seeds/DatabaseSeeder.php
@@ -4,6 +4,36 @@ class DatabaseSeeder extends Seeder {
 
 	public function run()
 	{
+            //$adminRole = Role::create(array('title'=>'admin'));
+            $adminRole = new Role;
+            $adminRole->title = "admin";
+            $adminRole->save();
+            $memberRole = new Role;
+            $memberRole->title = 'member';
+            $memberRole->save();
+            
+            $user = new User;
+            $user->username = "Mayank";
+            $user->email = "mayankasthana1993@gmail.com";
+            $user->password = "asdfjkl;";
+            $user->save();
+            $user->role()->attach($adminRole);
+            
+            $user = new User;
+            $user->username = "Shivam";
+            $user->email = "shivam@kratee.com";
+            $user->password = ";lkjfdsa";
+            $user->save();
+            $user->role()->attach($memberRole);
+
+            $user = new User;
+            $user->username = "Mihir";
+            $user->email = "mihir@gmail.com";
+            $user->password = "password";
+            $user->save();
+            $user->role()->attach($memberRole);
+
+            
 		Eloquent::unguard();
 	}
 }
