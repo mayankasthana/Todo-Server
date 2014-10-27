ee4070edb91530bee07bbbdff05df61af54bea49
mayank <mayankasthana1993@gmail.com>
2014-10-28 03:49:43 +0530

    Added notify method

--- app/models/Notification.php
+++ app/models/Notification.php
@@ -2,7 +2,16 @@
 
 class Notification extends Eloquent {
 
-	protected $table = 'notifications';
-	public $timestamps = true;
+    protected $table = 'notifications';
+    public $timestamps = true;
+
+    public static function notify($toUserId, $message, $type = null, $origin = null) {
+        $notification =  new Notification;
+        $notification->origin = $origin;
+        $notification->to_user_id = $toUserId;
+        $notification->message = $message;
+        $notification->type = $type;
+        $notification->save();
+    }
 
 }
